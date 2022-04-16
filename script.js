ad5f90f3226f4040d5fab0df3756765a4af54817
stephmarisa <s.chui@wustl.edu>
2022-04-16 13:30:54 -0700

    latest version

---
+++ script.js
@@ -0,0 +1,16 @@
+var eyeBall = document.querySelector('#eyeBall');
+
+document.onmousemove = (event) => {
+    var x = event.clientX * 100 / window.innerWidth + "%";
+    var y = event.clientY * 100 / window.innerHeight + "%";
+    
+    eyeBall.style.transition = "0s";
+    eyeBall.style.left = x;
+    eyeBall.style.top = y;
+}
+
+document.onmouseout = (event) => {
+    eyeBall.style.transition = "0.7s";
+    eyeBall.style.left = "50%";
+    eyeBall.style.top = "50%";
+}
